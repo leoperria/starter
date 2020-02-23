9f7cd87ebb5926b2a513152a4c03d309b25ec160
Leonardo Perria <leo.perria@stelltec.com>
2020-02-23 19:05:57 +0000

    Fix  lint issues

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,15 +23,15 @@ const schema = buildSchema(`
 // The root provides a resolver function for each API endpoint
 const root = {
     hello: () => {
-        return 'Hello world!';
+        return "Hello world!";
     },
 };
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
+app.use("/graphql", graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
